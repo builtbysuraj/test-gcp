af6bb59575d3f1996338f855b64e21eec4c7d45d
builtbysuraj <surajgupta3553@gmail.com>
2025-02-21 12:16:17 +0530

    Add POST req

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import express from 'express'
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.json({ data: 'Hello there' })
+})
+
+app.post('/form', (req, res) => {
+  const data = req.body
+  res.json({ data })
 })
 
 app.listen(PORT, () => console.log(`Server is running in ${PORT} `))
